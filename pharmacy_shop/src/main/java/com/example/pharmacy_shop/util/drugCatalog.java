7851c0b6856f111e5483014032dabf889eb0aaca
George <georgeey123@gmail.com>
2023-07-19 11:53:52 +0000

    made changes

---
+++ pharmacy_shop/src/main/java/com/example/pharmacy_shop/util/drugCatalog.java
@@ -0,0 +1,34 @@
+
+package com.example.pharmacy_shop.util;
+
+import java.util.ArrayList;
+
+public class DrugCatalog {
+
+    private ArrayList<Drug> drugList;
+
+
+    public DrugCatalog(){
+        this.drugList = new ArrayList<Drug>();
+    }
+    public ArrayList<Drug> getDrugList() {
+        return drugList;
+    }
+
+    public void setDrugList(ArrayList<Drug> drugList) {
+        this.drugList = drugList;
+    }
+
+    public Drug addDrugs(Drug d) {
+        // Drug d = new Drug();
+        drugList.add(d);
+        return d;
+
+    }
+    public void removeDrug(Drug d)
+    {
+        drugList.remove(d);
+    }
+
+
+}
