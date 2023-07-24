1bbbac25abc25721779daf688a17390425af81b2
Kwasiob <100800474+Kwasiob@users.noreply.github.com>
2023-07-24 10:03:19 +0000

    Delete pharmacy_shop directory

--- pharmacy_shop/src/main/java/com/example/pharmacy_shop/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.pharmacy_shop {
-    requires javafx.controls;
-    requires javafx.fxml;
-            
-                            
-    opens com.example.pharmacy_shop to javafx.fxml;
-    exports com.example.pharmacy_shop;
-}
